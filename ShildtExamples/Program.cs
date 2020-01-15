211206b7ea5beeb14bae4e5d518611dcae019113
Conterry <glebois05@gmail.com>
2020-01-15 22:05:38 +0200

    ...

---
+++ ShildtExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace ShildtExamples
+{
+    // Пример заполнения класса.
+    class Building
+    {
+        public int Floors;
+        public int Area;
+        public int Occupants; 
+    }
+
+    class BuildingDemo
+    {
+        static void Main()
+        {
+            Building house = new Building();
+            int areaPP;
+
+            house.Occupants = 4;
+            house.Area = 2500;
+            house.Floors = 2;
+
+            areaPP = house.Area / house.Occupants;  
+            Console.WriteLine(areaPP);
+        }
+    }
+}
